123c03b437eaf00652241e15acbd5c011775c4f7
pmfmavisi <pmfmavisi@gmail.com>
2025-07-21 21:08:03 +0300

    update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
-from flask import Flask
 from flask_cors import CORS
 import os
 import pyodbc
@@ -65,4 +64,5 @@ def messages():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
